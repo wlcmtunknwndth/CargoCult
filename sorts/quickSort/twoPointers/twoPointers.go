3f5541dc6dd70487d703aa552c3ece0c200da71f
Артем Петров <fittedorangeofficial@mail.ru>
2024-07-08 23:29:31 +0300

    first commit

---
+++ sorts/quickSort/twoPointers/twoPointers.go
@@ -0,0 +1,41 @@
+package twoPointers
+
+func QuickSortStart[K comparable](arr []K, cmp func(K, K) int) {
+	quickSort(arr, 0, len(arr)-1, cmp)
+}
+
+func quickSort[K comparable](arr []K, l, r int, cmp func(K, K) int) {
+	if len(arr) < 2 {
+		return
+	}
+	if l < r {
+		part := partition(arr, l, r, cmp)
+		if part == -1 {
+			return
+		}
+		quickSort(arr, l, part, cmp)
+		quickSort(arr, part+1, r, cmp)
+	}
+}
+
+// -1 if left is greater, 0 if equal, 1 if right is greater
+func partition[K comparable](arr []K, l, r int, cmp func(K, K) int) int {
+	if len(arr) == 0 || len(arr) == 1 {
+		return -1
+	}
+
+	mid := (l + r) / 2
+
+	for l < r {
+		for cmp(arr[l], arr[mid]) == 1 {
+			l++
+		}
+		for cmp(arr[r], arr[mid]) == -1 {
+			r--
+		}
+		arr[l+1], arr[r-1] = arr[r-1], arr[l+1]
+		l++
+		r--
+	}
+	return r
+}
---
+++ sorts/quickSort/twoPointers/twoPointers_test.go
@@ -0,0 +1,43 @@
+package twoPointers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var testCases = []struct {
+	given    []int
+	expected []int
+}{
+	{
+		given:    []int{0, 42, 1, 31, 123, 4, 5, 3, 1},
+		expected: []int{0, 1, 1, 3, 4, 5, 31, 42, 123},
+	},
+	{
+		given:    []int{3, 1, 2, 1, 15, 14, 13},
+		expected: []int{1, 1, 2, 3, 13, 14, 15},
+	},
+	{
+		given:    []int{0, 1, 2, 3, 4},
+		expected: []int{0, 1, 2, 3, 4},
+	},
+}
+
+func TestTwoPointers(t *testing.T) {
+	for _, tc := range testCases {
+		var tmp []int
+		copy(tmp, tc.given)
+		QuickSortStart(tmp, func(i1 int, i2 int) int {
+			if i1 > i2 {
+				return -1
+			} else if i1 == i2 {
+				return 0
+			} else {
+				return 1
+			}
+		})
+
+		assert.
+
+	}
+}
